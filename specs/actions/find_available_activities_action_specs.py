a149511a99e37f96bb7edfbcb43c4a780f1a543e
regiluze <regiluze@hotmail.com>
2018-08-08 00:11:43 +0200

    Refactor

--- specs/actions/find_available_activities_action_specs.py
+++ specs/actions/find_available_activities_action_specs.py
@@ -12,28 +12,25 @@ FAKE_GEOJSON_RESULT = 'irrelevant-result'
 
 with describe('find available activities action specs'):
     with before.each:
-         pass
+        self.activities_service_stub = Stub()
+        self.geoJson_formater = Stub()
+        self.find_activities_action = FindActivitiesAction(self.activities_service_stub, self.geoJson_formater)
+
     with context('when no filter is provided'):
         with it('returns all available activities in geoJson format'):
-            activities_service_stub = Stub()
-            when(activities_service_stub).find_by(ANY_ARG).returns(FAKE_ACTIVITY_LIST_SAMPLE)
-            geoJson_formater = Stub()
-            when(geoJson_formater).to_feature_collection(FAKE_ACTIVITY_LIST_SAMPLE).returns(FAKE_GEOJSON_RESULT)
-            find_activities_action = FindActivitiesAction(activities_service_stub, geoJson_formater)
+            when(self.activities_service_stub).find_by(ANY_ARG).returns(FAKE_ACTIVITY_LIST_SAMPLE)
+            when(self.geoJson_formater).to_feature_collection(FAKE_ACTIVITY_LIST_SAMPLE).returns(FAKE_GEOJSON_RESULT)
 
-            all_available_activities = find_activities_action.execute()
+            all_available_activities = self.find_activities_action.execute()
 
             expect(all_available_activities).to(equal(FAKE_GEOJSON_RESULT))
 
     with context('when filter is provided'):
         with it('returns filtered available activities in geoJson format'):
             filter = {}
-            activities_service_stub = Stub()
-            when(activities_service_stub).find_by(filter).returns(FAKE_ACTIVITY_LIST_SAMPLE)
-            geoJson_formater = Stub()
-            when(geoJson_formater).to_feature_collection(FAKE_ACTIVITY_LIST_SAMPLE).returns(FAKE_GEOJSON_RESULT)
-            find_activities_action = FindActivitiesAction(activities_service_stub, geoJson_formater)
+            when(self.activities_service_stub).find_by(filter).returns(FAKE_ACTIVITY_LIST_SAMPLE)
+            when(self.geoJson_formater).to_feature_collection(FAKE_ACTIVITY_LIST_SAMPLE).returns(FAKE_GEOJSON_RESULT)
 
-            all_available_activities = find_activities_action.execute(filter)
+            all_available_activities = self.find_activities_action.execute(filter)
 
             expect(all_available_activities).to(equal(FAKE_GEOJSON_RESULT))
