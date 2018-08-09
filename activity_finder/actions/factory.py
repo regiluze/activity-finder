6f1968e4fc552e270a812c076dc36799ad5d57a9
regiluze <regiluze@hotmail.com>
2018-08-09 15:00:53 +0200

    Add recommend activity action factory method
    Factory method added for this action

--- activity_finder/actions/factory.py
+++ activity_finder/actions/factory.py
@@ -5,6 +5,7 @@ from infcommon import Factory
 from activity_finder.model import factory as model_factory
 
 from activity_finder.actions.find_activities import FindActivitiesAction
+from activity_finder.actions.recommend_activity import RecommendActivityAction
 
 
 
@@ -12,4 +13,10 @@ def find_activities_action():
     return Factory.instance('find_activities_action',
             lambda: FindActivitiesAction(model_factory.activities_service(),
                                          model_factory.geojson_formater())
+            )
+
+def recommend_activity_action():
+    return Factory.instance('recommend_activity_action',
+            lambda: RecommendActivityAction(model_factory.activities_service(),
+                                            model_factory.geojson_formater())
             )
